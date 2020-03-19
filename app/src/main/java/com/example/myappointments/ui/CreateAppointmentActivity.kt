df0a3424b5eea5c666d5cac0c0e7aa2e2d5806ab
Agustin <agustin.e.blanco@gmail.com>
2020-03-18 23:28:15 -0300

    Fix scheduled date

--- app/src/main/java/com/example/myappointments/ui/CreateAppointmentActivity.kt
+++ app/src/main/java/com/example/myappointments/ui/CreateAppointmentActivity.kt
@@ -209,7 +209,8 @@ class CreateAppointmentActivity : AppCompatActivity() {
         val listener = DatePickerDialog.OnDateSetListener { datePicker, y, m, d ->
            // Toast.makeText(this, "$y-$m-$d", Toast.LENGTH_SHORT).show()
             selectedCalendar.set(y,m,d)
-            etScheduledDate.setText(resources.getString(R.string.date_format, y, m.twoDigits(), d.twoDigits()))
+
+            etScheduledDate.setText(resources.getString(R.string.date_format, y, (m+1).twoDigits(), d.twoDigits()))
             etScheduledDate.error = null
             displayRadioButtons()
         }
